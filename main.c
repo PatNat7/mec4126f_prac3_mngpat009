5b4d1da4eb6dcd30417b09af18e94fdfa640a2c0
PatNat7 <109956007+PatNat7@users.noreply.github.com>
2023-03-07 16:41:26 +0200

    3.1 age_data Struct
    Struct containing variables called day, month, year and age to store age_data

--- main.c
+++ main.c
@@ -7,9 +7,20 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include "stdio.h"
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
+/*struct contain age data*/
 
+typedef struct age_data{
+
+	uint8_t day;
+	uint8_t month;
+	uint16_t year;
+	uint8_t age;
+}age_data;
+
+age_data my_age;   //create age_data struct called my_age
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
